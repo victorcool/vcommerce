45710bbee979fbe540ec4f401c9701e48bcfabef
victorcool <ezeanivictor.c@gmail.com>
2019-08-09 01:00:23 +0100

    new update

--- app/Http/Controllers/admin/SettingsController.php
+++ app/Http/Controllers/admin/SettingsController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use view;
 use App\Setting;
+use DB;
+use Validator;
+use Redirect;
+use Session;
 
 class SettingsController extends Controller
 {
@@ -72,7 +76,37 @@ class SettingsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $rules = [
+            'first_email' => 'required',
+            'first_number' => 'required',
+            'facebook' => 'required',
+            'instagram' => 'required',
+            'site_title' => 'required'
+        ];
+
+        $validator = validator::make($request->all(),$rules);
+        if($validator->fails()){
+            return redirect()->back()->withInput()->withErrors($validator);
+        }else{
+            try {
+                // let update start
+                $setting = Setting::find($id);
+                $setting->title = $request->input('site_title');
+                $setting->email1 = $request->input('first_email');
+                $setting->email2 = $request->input('second_email');
+                $setting->phone1 = $request->input('first_number');
+                $setting->phone2 = $request->input('second_number');
+                $setting->facebook = $request->input('facebook');
+                $setting->instagram = $request->input('instagram');
+                $setting->twitter = $request->input('twitter');
+                $setting->address = $request->input('address');
+                $setting->save();
+                return redirect()->back()->with('success','Settings Updated');
+            } catch (\Throwable $th) {
+                $error =  Session::flash('error', 'Sorry, operation could not be completed.'.$th->getMessage());
+                return redirect()->back()->with($error);
+            }
+        }
     }
 
     /**
--- resources/views/administrator/assets/inc/forms/settingsReadOnly.blade.php
+++ resources/views/administrator/assets/inc/forms/settingsReadOnly.blade.php
@@ -6,27 +6,42 @@
                 <div class="row">
                   <div class="col-md-12">
                     <div class="form-group">
-                        <label for="category_name">Site title</label>
+                        <label for="site_title {{ ($errors->has('site_title')) ? 'has-error' : '' }}">Site title</label>
                         <input type="text" name="site_title" value="{{$setting->title}}" class="form-control" id="Title" >
                     </div>
+                    @if($errors->has('site_title'))
+                    <span class="help-block">
+                      {{ $errors->first('site_title') }}
+                    </span>
+                  @endif
                   </div>
                   <div class="col-md-12">
                         <div class="form-group">
-                            <label for="second_mail">First email (compulsory)</label>
-                            <input type="text" name="second_mail" value="{{$setting->email1}}" class="form-control" id="Title" >
+                            <label for="first_email {{ ($errors->has('first_email')) ? 'has-error' : '' }}">First email (compulsory)</label>
+                            <input type="text" name="first_email" value="{{$setting->email1}}" class="form-control" id="Title" >
                         </div>
+                        @if($errors->has('first_email'))
+                        <span class="help-block">
+                          {{ $errors->first('first_email') }}
+                        </span>
+                      @endif
                   </div> 
                   <div class="col-md-12">
                         <div class="form-group">
                             <label for="second_mail">Second email (Optional)</label>
-                            <input type="text" name="second_mail" value="{{$setting->email2}}" class="form-control" id="Title" >
+                            <input type="text" name="second_email" value="{{$setting->email2}}" class="form-control" id="Title" >
                         </div>
                   </div> 
                     <div class="col-md-12">
-                        <div class="form-group">
+                        <div class="form-group {{ ($errors->has('first_number')) ? 'has-error' : '' }}">
                             <label for="first_number">First PhoneNumber (Compulsory)</label>
                             <input type="text" name="first_number" value="{{$setting->phone1}}" class="form-control" id="Title" >
                         </div>
+                        @if($errors->has('first_number'))
+                        <span class="help-block">
+                          {{ $errors->first('first_number') }}
+                        </span>
+                      @endif
                     </div>
                     <div class="col-md-12">
                         <div class="form-group">
@@ -36,15 +51,25 @@
                     </div> 
                     <div class="col-md-12">
                         <div class="form-group">
-                            <label for="facebook">facebook (optional)</label>
+                            <label for="facebook {{ ($errors->has('facebook')) ? 'has-error' : '' }}">facebook (optional)</label>
                             <input type="text" name="facebook" value="{{$setting->facebook}}" class="form-control" id="Title" >
                         </div>
+                        @if($errors->has('facebook'))
+                            <span class="help-block">
+                              {{ $errors->first('facebook') }}
+                            </span>
+                          @endif
                     </div>  
                     <div class="col-md-12">
                         <div class="form-group">
-                            <label for="instagram">Instagram(optional)</label>
-                            <input type="text" name="second_number" value="{{$setting->instagram}}" class="form-control" id="Title" >
+                            <label for="instagram {{ ($errors->has('instagram')) ? 'has-error' : '' }}">Instagram(optional)</label>
+                            <input type="text" name="instagram" value="{{$setting->instagram}}" class="form-control" id="Title" >
                         </div>
+                        @if($errors->has('instagram'))
+                            <span class="help-block">
+                              {{ $errors->first('instagram') }}
+                            </span>
+                          @endif
                     </div> 
                     <div class="col-md-12">
                         <div class="form-group">
